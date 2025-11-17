1b44052794faf59ec2e465768cfd8f7ce706e2ed
Aishwarya <aishwaryagawande@Aishwaryas-MacBook-Air.local>
2025-11-17 23:32:33 +0530

    Changes done in main class

--- src/mainPackage/MainClass.java
+++ src/mainPackage/MainClass.java
@@ -5,5 +5,6 @@ public class MainClass {
 
          System.out.println("Name plz : My Name is Aishwarya Gawande");
          System.out.println("I am doing automation revision session");
+         System.out.println("This is my own established branch and repo");
     }
 }
