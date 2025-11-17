e71f5a2ee97dd66dd31f9d22cfb4273f063f09e7
Aishwarya <aishwaryagawande@Aishwaryas-MacBook-Air.local>
2025-11-17 23:20:10 +0530

    Changes done in code

--- src/mainPackage/MainClass.java
+++ src/mainPackage/MainClass.java
@@ -4,5 +4,6 @@ public class MainClass {
      public static void main (String[] args) {
 
          System.out.println("Name plz : My Name is Aishwarya Gawande");
+         System.out.println("I am doing automation revision session");
     }
 }
